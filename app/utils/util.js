c92623687343816d5453dea7196e6d0c10330a9f
JungEnpark <jungun.park@makeus.com>
2016-05-02 20:40:00 +0900

    디버깅 코드 삽입

--- app/utils/util.js
+++ app/utils/util.js
@@ -170,7 +170,8 @@ export default {
 
   _getRequestServiceType(type) {
     let host = localStorage.getItem('ls.Host')
-    if(host) {
+    console.log(host)
+    if(host === undefined) {
       host = 'dev-admin.dingo.tv'
     }
 
